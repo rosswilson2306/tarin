297766351ace505bc2c35aaab230d5e424219bf5
Ross Wilson <ross.wilson@homeflow.co.uk>
2025-03-03 20:51:13 +0000

    add initial instructions to readme and helper function to read list of websites from text file for development

--- README.md
+++ README.md
@@ -3,4 +3,14 @@
 A tool that automates Lighthouse reports for all relevant URLs on for a given 
 list of websites.
 
+## Getting Started
 
+Add a `.env` file to the root of the project and add the following variables:
+
+```
+PSI_KEY=XXXXX
+PSI_URL=XXXXX
+```
+
+At the time of writing the url for the Page Speed Insights api is `https://www.googleapis.com/pagespeedonline/v5/runPagespeed`
+but this may change in the future (see [Page Speed Insights documentation](https://developers.google.com/speed/docs/insights/v5/get-started)).
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use client::PsiClient;
 use dotenv::dotenv;
 use std::error::Error;
+use tokio::fs::File;
+use tokio::io::{self, AsyncBufReadExt, BufReader};
 
 mod client;
 
@@ -11,9 +13,25 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let psi_url = std::env::var("PSI_URL")?;
     let psi_key = std::env::var("PSI_KEY")?;
 
+    let _websites = get_base_sites("sites.txt").await?;
+
     let client = PsiClient::new(&psi_url, &psi_key);
 
     let _report = client.get_report("https://google.com").await?;
 
     Ok(())
+}
+
+// Dev helper to get website list from file
+async fn get_base_sites(filepath: &str) -> io::Result<Vec<String>> {
+    let file = File::open(filepath).await?;
+    let reader = BufReader::new(file);
+    let mut lines = reader.lines();
+    let mut sites = Vec::new();
+
+    while let Some(line) = lines.next_line().await? {
+        sites.push(line)
+    }
+
+    Ok(sites)
 }
