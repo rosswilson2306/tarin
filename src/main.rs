1d4fab83020a4080b352b79b4d35c1031cea8113
Ross Wilson <ross.wilson@homeflow.co.uk>
2025-03-13 18:37:02 +0000

    set up axum server with sse stream handler sending fixed message in 2s intervals

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-use anyhow::Result;
+use std::{convert::Infallible, time::Duration};
+
+use anyhow::{Context, Result};
+use axum::{
+    response::sse::{Event, KeepAlive, Sse},
+    routing::get,
+    Router,
+};
 use dotenv::dotenv;
 use sitemaps::extract_sitemap_url_list;
-use tokio::fs::File;
 use tokio::io::{self, AsyncBufReadExt, BufReader};
+use tokio::{fs::File, time::interval};
+use tokio_stream::StreamExt as _;
 
 mod client;
 mod sitemaps;
@@ -11,8 +19,9 @@ mod sitemaps;
 async fn main() -> Result<()> {
     dotenv().ok();
 
-    let _psi_url = std::env::var("PSI_URL")?;
-    let _psi_key = std::env::var("PSI_KEY")?;
+    let _psi_url = std::env::var("PSI_URL").context("PSI_URL not found")?;
+    let _psi_key = std::env::var("PSI_KEY").context("PSI_KEY not found")?;
+    let server_url = std::env::var("SERVER_URL").context("SERVER_URL not found")?;
 
     let websites = get_base_sites("sites.txt").await?;
 
@@ -20,7 +29,19 @@ async fn main() -> Result<()> {
         extract_sitemap_url_list(url).await?;
     }
 
+    let app = Router::new().route("/reports", get(sse_reports_hanlder));
+
+    let listener = tokio::net::TcpListener::bind(server_url).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+
     Ok(())
+}
+
+async fn sse_reports_hanlder() -> Sse<impl futures::Stream<Item = Result<Event, Infallible>>> {
+    let stream = tokio_stream::wrappers::IntervalStream::new(interval(Duration::from_secs(2)))
+        .map(|_| Ok(Event::default().data("New report available")));
+
+    Sse::new(stream).keep_alive(KeepAlive::default())
 }
 
 // Dev helper to get website list from file
