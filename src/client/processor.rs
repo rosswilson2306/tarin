a4e460b3bbe26c3309fb6619ab45aa32347ad0b3
Ross Wilson <ross.wilson@homeflow.co.uk>
2025-04-06 12:11:49 +0100

    refactor processor to use match block in variable setting to avoid nesting

--- src/client/processor.rs
+++ src/client/processor.rs
@@ -30,37 +30,43 @@ pub async fn process_websites(
         let psi_client = psi_client.clone();
 
         task::spawn(async move {
-            if let Ok(site_urls) = extract_sitemap_url_list(&site).await {
-                for url in site_urls {
-                    println!("Running PSI report for: {} ...", url);
+            let site_urls = match extract_sitemap_url_list(&site).await {
+                Ok(urls_list) => urls_list,
+                Err(_) => {
+                    return eprintln!(
+                        "Failed to extract urls from sitemaps for: {}",
+                        site.as_ref()
+                    )
+                }
+            };
 
-                    let psi_res = match psi_client.get_report(url.as_ref()).await {
-                        Ok(res) => res,
-                        Err(_) => {
-                            eprintln!("Error fetching report for: {}", url);
-                            break;
-                        }
-                    };
+            for url in site_urls {
+                println!("Running PSI report for: {} ...", url);
 
-                    let psi_event = match Event::default().json_data(psi_res) {
-                        Ok(event) => event,
-                        Err(_) => {
-                            eprintln!("Error creating event from PSI report data for: {}", url);
-                            break;
-                        }
-                    };
-
-                    if sender.send(Ok(psi_event)).await.is_err() {
+                let psi_res = match psi_client.get_report(url.as_ref()).await {
+                    Ok(res) => res,
+                    Err(_) => {
+                        eprintln!("Error fetching report for: {}", url);
                         break;
                     }
+                };
 
-                    sleep(Duration::from_secs(2)).await;
+                let psi_event = match Event::default().json_data(psi_res) {
+                    Ok(event) => event,
+                    Err(_) => {
+                        eprintln!("Error creating event from PSI report data for: {}", url);
+                        break;
+                    }
+                };
+
+                if sender.send(Ok(psi_event)).await.is_err() {
+                    break;
                 }
-            } else {
-                eprintln!("Failed to extract urls from sitemaps")
+
+                sleep(Duration::from_secs(2)).await;
             }
         });
     }
 
-    todo!()
+    Ok(())
 }
